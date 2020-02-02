64a35adf3951912df8bf79de66831890e00cb5dc
Nemanja Gligovic <46631210+Slashh48@users.noreply.github.com>
2020-02-02 17:22:49 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function myFunctionToggle(y) {
+    y.classList.toggle("change");
+  }
+
+
+  function myFunction() {
+        
+    var x = document.getElementById("nav-menu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+    
+    
+}
